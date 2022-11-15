6e9fb027f4b26842224d42542120752f84b290e0
Sudharshanam <sudharshanam@csu.fullerton.edu>
2022-11-15 13:37:48 -0800

    First commit

---
+++ file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pwd
+cat /etc/os-release
+git version
+java --version
+jenkins --version
+date
+echo "This is the end of the world "
+echo "******************************"
+sudo yum makecache --refresh
+sudo yum -y install cowsay.noarch
+cowsay -e @@ Fuck you
