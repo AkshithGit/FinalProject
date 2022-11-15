9466c764f799487b44f544e309b2311daf8f933f
Sudharshanam <sudharshanam@csu.fullerton.edu>
2022-11-15 13:47:04 -0800

    Edited cowsay command

--- file.sh
+++ file.sh
@@ -7,6 +7,5 @@ jenkins --version
 date
 echo "This is the end of the world "
 echo "******************************"
-sudo yum makecache --refresh
-sudo yum -y install cowsay.noarch
+sudo yum -y install cowsay
 cowsay -e @@ Fuck you
